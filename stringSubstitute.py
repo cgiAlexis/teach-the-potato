b95a5b8b0ad1ad844e77014e3058e287475f4031
cgiAlexis <cgiAlexis@gmail.com>
2020-02-12 10:17:58 +0000

    Fixed string issues and understood embedded strings

--- stringSubstitute.py
+++ stringSubstitute.py
@@ -3,5 +3,6 @@ jypsum = "jypsum"
 print('%s is a mineral found in the ground' % agate)
 print('%s is a crystal found in mineral formations' % jypsum)
 
-'''MS VS Code can't handle this double string'''
-print('%s and %s are equally valuable' % agate % jypsum)
+'''Refer to embedded values to get this to work'''
+grand = '%s and %s are equally valuable'
+print( grand % (agate, jypsum))
--- strings.py
+++ strings.py
@@ -1,10 +1,8 @@
 '''
 Strings can't appear the same as variables, so they must be quoted
-What to do about the double string?
 '''
 
 agate = 'agate'
 jypsum = 'jypsum'
 print('%s is a mineral found in the ground' % agate)
 print('%s is a crystal found in mineral formations' % jypsum)
-print('%s and %s are equally valuable' % agate % jypsum)

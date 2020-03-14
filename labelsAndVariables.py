fa88a6f7b923448a07604f04b40297dc468c66d2
cgiAlexis <7907361+cgiAlexis@users.noreply.github.com>
2020-03-14 15:57:42 +0000

    Neater explanation in output

--- labelsAndVariables.py
+++ labelsAndVariables.py
@@ -1,5 +1,6 @@
+phrase = "all_the_coins = %s"
 found_coins = 23
 magic_coins = 7
 stolen_coins = 55
 all_the_coins = found_coins + magic_coins * 365 - stolen_coins * 52
-print(all_the_coins)
+print(phrase % all_the_coins)
